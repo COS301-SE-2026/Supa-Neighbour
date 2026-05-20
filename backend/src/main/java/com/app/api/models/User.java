c53340591f6843a5e06e97fe367ddd06bebdd692
AKT-TECH49-DEV <amantletemo29@gmail.com>
2026-05-20 22:23:54 +0200

    fix: user entity has been updated and now able to map issue had to do with Hibernate and us using the exact column names in the db schema

--- backend/src/main/java/com/app/api/models/User.java
+++ backend/src/main/java/com/app/api/models/User.java
@@ -1,142 +1,92 @@
 package com.app.api.models;
 
 import java.sql.Date;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+
+
 @Entity
 @Table(name = "UserTable")
-public class User 
+public class User
 {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UserID") //basically u use the column anntotation to make the column names in shema.sql
     private int id;
-    private String firstName;
-    private String lastName;
+
+    @Column(name = "UserPassword")
     private String password;
+
+    @Column(name = "UserName")
+    private String firstName;
+
+    @Column(name = "UserSurname")
+    private String lastName;
+
+    @Column(name = "UserEmail")
     private String email;
+
+    @Column(name = "UserPhoneNumber")
     private String phoneNumber;
-    private Date dateOfBirth;
+
+    @Column(name = "UserGender")
     private String gender;
+
+    @Column(name = "UserDOB")
+    private Date dateOfBirth;
+
+    @Column(name = "UserAddressID")
     private int addressId;
+
+    @Column(name = "UserBadgeID")
     private String badgeId;
+
+    @Column(name = "UserRatingID")
     private String ratingId;
+
+    @Column(name = "UserTypeID")
     private String typeId;
 
-    public User() {
-    }
+    public User() {}
 
-    public User(int id, String firstName, String lastName, String password, String email, String phoneNumber, Date dateOfBirth, String gender, int addressId, String badgeId, String ratingId, String typeId) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
-        this.dateOfBirth = dateOfBirth;
-        this.gender = gender;
-        this.addressId = addressId;
-        this.badgeId = badgeId;
-        this.ratingId = ratingId;
-        this.typeId = typeId;
-    }
+    /// getters and setters: 
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public String getFirstName() {
-        return firstName;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public String getLastName() {
-        return lastName;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getPassword() {
-        return password;
-    }  
-    public String getEmail() {
-        return email;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
+    public String getPhoneNumber() { return phoneNumber; }
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
 
-    public String getGender() {
-        return gender;      
-    }
+    public String getGender() { return gender; }
+    public void setGender(String gender) { this.gender = gender; }
 
-    public int getAddressId() {
-        return addressId;
-    }
+    public Date getDateOfBirth() { return dateOfBirth; }
+    public void setDateOfBirth(Date dateOfBirth) { this.dateOfBirth = dateOfBirth; }
 
-    public String getBadgeId() {
-        return badgeId;
-    }
+    public int getAddressId() { return addressId; }
+    public void setAddressId(int addressId) { this.addressId = addressId; }
 
-    public String getRatingId() {
-        return ratingId;
-    }
+    public String getBadgeId() { return badgeId; }
+    public void setBadgeId(String badgeId) { this.badgeId = badgeId; }
 
-    public String getTypeId() {
-        return typeId;
-    }
+    public String getRatingId() { return ratingId; }
+    public void setRatingId(String ratingId) { this.ratingId = ratingId; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
-    }
-
-    public void setBadgeId(String badgeId) {
-        this.badgeId = badgeId;
-    }
-
-    public void setRatingId(String ratingId) {
-        this.ratingId = ratingId;
-    }
-
-    public void setTypeId(String typeId) {
-        this.typeId = typeId;
-    }
+    public String getTypeId() { return typeId; }
+    public void setTypeId(String typeId) { this.typeId = typeId; }
 }
