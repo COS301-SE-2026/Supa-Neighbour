d9f00ac700766a413920877dab995966f62ac41f
Divo <u24607526@tuks.co.za>
2026-06-01 19:58:55 +0200

    WIP: save backend-api-db progress

--- backend/src/main/java/com/app/api/models/User.java
+++ backend/src/main/java/com/app/api/models/User.java
@@ -4,6 +4,8 @@ import java.sql.Date;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -91,8 +93,8 @@ public class User {
      * Sets the user ID.
      * @param id the user ID
      */
-    public void setId(int id) {
-        this.id = id;
+    public void setUserid(int userid) {
+        this.userid = userid;
     }
 
     public void setFirstName(String firstName) {
--- backend/src/main/java/com/app/api/services/UserService.java
+++ backend/src/main/java/com/app/api/services/UserService.java
@@ -2,7 +2,7 @@ package com.app.api.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import com.app.api.models.User;
 import com.app.api.repositories.UserRepository;
 
