6f46363c07fcb35b77344a03526bd9261888c674
Michelle Njoroge <micheyn83uni@gmail.com>
2026-07-10 21:54:18 +0200

    fixed import paths after folder restructure

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'screens/splash_screen.dart';
+import 'screens/auth/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
