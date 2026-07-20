c6a3ec12d3a4aa0ac539041bceff3938f7dc1a59
Divo <u24607526@tuks.co.za>
2026-07-20 09:39:05 +0200

    h

--- backend/src/main/java/com/app/api/services/FirebaseAuthService.java
+++ backend/src/main/java/com/app/api/services/FirebaseAuthService.java
@@ -1,6 +1,5 @@
 package com.app.api.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
@@ -17,9 +16,13 @@ import com.app.api.repositories.UserRepository;
 @Service
 public class FirebaseAuthService {
 
-    @Autowired
-    private UserRepository userRepository;
+    
+    private  final UserRepository userRepository;
 
+
+    public FirebaseAuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
        /**
      * Verifies a Firebase ID token.
      *
