10f01db571324b6dd6bc224499911f8d39a9c3ac
AKT-TECH49-DEV <amantletemo29@example.com>
2026-07-22 17:35:27 +0200

    get location via neighbourhood

--- backend/src/main/java/com/app/api/repositories/LocationRepository.java
+++ backend/src/main/java/com/app/api/repositories/LocationRepository.java
@@ -1,14 +1,22 @@
 package com.app.api.repositories;
 
+import com.app.api.models.Location;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import com.app.api.models.Location;
+import java.util.Optional;
 
 /**
  * Repository for Location entities.
  */
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
-    
+    /**
+     * Finds a Location entity by its neighbourhood name.
+     */
+    @Query("SELECT l FROM Location l WHERE l.neighbourhoodName = :name")
+Optional<Location> findByNeighbourhoodName(@Param("name") String name);
 }
