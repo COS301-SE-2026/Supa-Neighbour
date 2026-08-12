2fa7f77f0ee24769116f5c0694ec014a600a72f3
michey-n <micheyn83uni@gmail.com>
2026-08-12 18:24:25 +0200

    Refactor: Updated TaskCompletionPage, TaskStartScreen, and AvailableHelpersScreen to use taskServiceProvider

--- frontend/lib/providers/service_providers.dart
+++ frontend/lib/providers/service_providers.dart
@@ -1,19 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/auth_service.dart';
-import '../services/profile_service.dart';
-import '../services/task_service.dart';
 
 // AUTH SERVICE PROVIDER
 final authServiceProvider = Provider<IAuthService>((ref) {
   return AuthService();
-});
-
-// PROFILE SERVICE PROVIDER
-final profileServiceProvider = Provider<IProfileService>((ref) {
-  return UserProfileService();
-});
-
-// TASK SERVICE PROVIDER
-final taskServiceProvider = Provider<ITaskService>((ref) {
-  return TaskService();
 });
