03452aec7c9ba49bfb08c9381973302a5370492a
michey-n <micheyn83uni@gmail.com>
2026-08-12 17:00:07 +0200

    Refactor: Updated task screens to use taskServiceProvider and fixed somee failing tests

--- frontend/lib/screens/tasks/task_detail_screen.dart
+++ frontend/lib/screens/tasks/task_detail_screen.dart
@@ -4,7 +4,6 @@ import '../../models/task_model.dart';
 import '../../constants/app_colors.dart';
 import 'edit_task_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/service_providers.dart';
 
 class TaskDetailScreen extends ConsumerStatefulWidget {
   final Task task;
