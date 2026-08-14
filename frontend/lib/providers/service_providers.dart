29804cc95bceb7a6cb5de1a6e687323bfd973756
michey-n <micheyn83uni@gmail.com>
2026-08-14 08:42:42 +0200

    Refactor: Added IBulletinService interface and provider for testability

--- frontend/lib/providers/service_providers.dart
+++ frontend/lib/providers/service_providers.dart
@@ -1,7 +1,25 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../services/auth_service.dart';
+import '../services/profile_service.dart';
+import '../services/task_service.dart';
+import '../services/bulletin_service.dart';
 
 // AUTH SERVICE PROVIDER
 final authServiceProvider = Provider<IAuthService>((ref) {
   return AuthService();
+});
+
+// PROFILE SERVICE PROVIDER
+final profileServiceProvider = Provider<IProfileService>((ref) {
+  return UserProfileService();
+});
+
+// TASK SERVICE PROVIDER
+final taskServiceProvider = Provider<ITaskService>((ref) {
+  return TaskService();
+});
+
+// BULLETIN SERVICE PROVIDER
+final bulletinServiceProvider = Provider<IBulletinService>((ref) {
+  return BulletinService();
 });
