e3ebab1bcb70f852ca1b495163b1133d4e3fb30f
Divo <u24607526@tuks.co.za>
2026-08-19 20:43:18 +0200

    checkstyle fine

--- backend/src/main/java/com/app/api/repositories/ReportRepository.java
+++ backend/src/main/java/com/app/api/repositories/ReportRepository.java
@@ -1,10 +1,10 @@
-package com.app.api.repositories;
-import com.app.api.models.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// package com.app.api.repositories;
+// import com.app.api.models.Report;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 
-public interface ReportRepository extends JpaRepository<Report, Integer>{
+// public interface ReportRepository extends JpaRepository<Report, Integer>{
     
-}
+// }
 
