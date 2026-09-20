349b541ef0b91e22fe00c4e02a5560d3f7e6acef
AKT-TECH49-DEV <amantletemo29@example.com>
2026-09-20 05:42:35 +0200

    add query to helper analytics repo

--- backend/src/main/java/com/app/api/repositories/HelperAnalyticsRepository.java
+++ backend/src/main/java/com/app/api/repositories/HelperAnalyticsRepository.java
@@ -1,10 +1,25 @@
 package com.app.api.repositories;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import com.app.api.models.HelperAnalytics;
- 
+
+import java.util.Optional;
+
 /**
- * Repository for HelperAnalytics entities.
+ * Repository for HelperAnalytics entities
  */
+@Repository
 public interface HelperAnalyticsRepository extends JpaRepository<HelperAnalytics, String> {
-    
+
+    /**
+     * Finds a HelperAnalytics entity by the associated user id.
+     * @param userId the id of the user
+     * @return an Optional containing the HelperAnalytics entity if found, or empty if not found
+     */
+    @Query("SELECT h FROM HelperAnalytics h WHERE h.userid.userid = :userId")
+    Optional<HelperAnalytics> findByUserId(@Param("userId") int userId);
 }
